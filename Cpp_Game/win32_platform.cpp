1cdd6f3aa56866d0ce4c02cda1dd70cfab5018f2
Corey <TaylorC39@cardiff.ac.uk>
2025-05-15 15:33:33 +0100

    Initial Commit

---
+++ Cpp_Game/win32_platform.cpp
@@ -0,0 +1,92 @@
+#include <windows.h>
+
+bool running = true;
+
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+Render_State render_state;
+
+#include "render_file.cpp"
+
+/* Callback function used to return the operation based on the message sent by the window. */
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{	
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		// Special case for closing the window.
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+		
+		// Case which returns the buffer size so that the correct amount of memory is reserved
+		// to render the graphics when the window size is changed by the user.
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			// Unsigned 32 bit intiger is reserved for an 8 bit word.
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+
+	}
+	return result;
+}
+
+/* Windows API is used for access to windows features. This function uses the API to create a 
+window and send messages to the callback function so that window features such as minimise and 
+quit can be processed. The game logic is also contained in this function. */
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = L"Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+
+	// Register Class
+	RegisterClass(&window_class);
+
+	// Create Window
+	HWND window = CreateWindow(window_class.lpszClassName, L"New Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
+
+	while (running) {
+		// Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		// Simulate
+		render_background();
+		clear_screen(0xffffff);
+		draw_rect(0, 0, 10, 10, 0xff0000);
+
+		// Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+	}
+}
+
